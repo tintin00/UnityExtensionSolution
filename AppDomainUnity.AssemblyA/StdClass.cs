6ab8c1823472bb3e0abdb7503032da86f6150333
Kim Min Kook <falseprophet@nate.com>
2013-12-11 13:55:52 +0900

    Create New Repository and Create New Project

---
+++ AppDomainUnity.AssemblyA/StdClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppDomainUnity.AssemblyA
+{
+    public class StdClass
+    {
+    }
+}
---
+++ UnityExtension/ExtensionConfigHandler/ExtensionConfigHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityExtension.ExtensionConfigHandler
+{
+    public class ExtensionConfigHandler
+    {
+    }
+}
